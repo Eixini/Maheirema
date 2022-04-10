6f125054e18f78e92199df25c44913728ef0a2c9
acidicMercury8 <acidicmercury8@outlook.com>
2022-04-10 21:54:59 +0300

    Add Avalonia application project
    Co-authored-by: Eixini <eixi16@gmail.com>

---
+++ src/Eixini.Maheirema/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Eixini.Maheirema
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

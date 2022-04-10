56ff3dc0574f0acf3cf439b612691e07c270bae7
acidicMercury8 <acidicmercury8@outlook.com>
2022-04-10 22:11:20 +0300

    Fix codestyle
    Co-authored-by: Eixini <eixi16@gmail.com>

--- src/Eixini.Maheirema/App.axaml.cs
+++ src/Eixini.Maheirema/App.axaml.cs
@@ -2,23 +2,18 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace Eixini.Maheirema
-{
-    public partial class App : Application
-    {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
+namespace Eixini.Maheirema;
+
+public partial class App : Application {
+    public override void Initialize() {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted() {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+            desktop.MainWindow = new MainWindow();
         }
 
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow();
-            }
-
-            base.OnFrameworkInitializationCompleted();
-        }
+        base.OnFrameworkInitializationCompleted();
     }
 }

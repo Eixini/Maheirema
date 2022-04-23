09be86b8c08bfbb06fdb68f9ff3373e033f0f8ea
Eixini <eixi16@gmail.com>
2022-04-23 21:04:47 +0300

    Move startup logic of WPF application to code behind

--- src/Eixini.Maheirema.Wpf/App.xaml.cs
+++ src/Eixini.Maheirema.Wpf/App.xaml.cs
@@ -5,5 +5,11 @@ namespace Eixini.Maheirema.Wpf {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }

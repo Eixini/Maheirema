024ae44651722f1319063f28103bed52f86d5475
Eixini <eixi16@gmail.com>
2022-04-23 16:12:05 +0300

    Add empty WPF project

---
+++ src/Eixini.Maheirema.Wpf/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Eixini.Maheirema.Wpf {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}

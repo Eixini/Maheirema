a80a0f3d0429db2708945a8ce905bf5919d34429
Eixini <eixi16@gmail.com>
2022-07-09 14:22:30 +0300

    Add missing files

---
+++ src/Eixini.Maheirema.Wpf/TagUserControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eixini.Maheirema.Wpf;
+    /// <summary>
+    /// Логика взаимодействия для TagUserControl.xaml
+    /// </summary>
+    public partial class TagUserControl : UserControl {
+        public TagUserControl() {
+            InitializeComponent();
+        }
+
+        public void TagContent(string text) {
+            TagContentLabel.Content = text;
+        }
+
+    public void TagDeleteButton_Click(object sender, RoutedEventArgs e) {
+        //MessageBox.Show("TagDeleteButton handler call! Tag: " + TagContentLabel.Content);
+        ((Button)TagGrid.Parent).Content = null;
+        ((Grid)TagDeleteButton.Parent).Children.Remove(this);
+
+    }
+
+}
+

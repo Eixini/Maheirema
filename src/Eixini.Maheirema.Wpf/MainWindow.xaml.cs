50931177fc961b0ec98340a18428c6338b453936
acidicMercury8 <acidicmercury8@outlook.com>
2022-07-09 14:45:37 +0300

    Fix codestyle

--- src/Eixini.Maheirema.Wpf/MainWindow.xaml.cs
+++ src/Eixini.Maheirema.Wpf/MainWindow.xaml.cs
@@ -1,31 +1,33 @@
-﻿using System.Windows;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Eixini.Maheirema.Wpf {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
+namespace Eixini.Maheirema.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void EntryLineTextBox_KeyDown(object sender, KeyEventArgs e) {
+        if (e.Key == Key.Enter) {
+            var ingredient = EntryLineTextBox.Text;
+
+            var testTag = new Button {
+                Content = ingredient
+            };
+
+            var testTagUC = new TagUserControl();
+            testTagUC.TagContent(ingredient);
+            TagFieldWrapPanel.Children.Add(testTagUC);
+
+            EntryLineTextBox.Clear();
         }
+    }
 
-        private void EntryLineTextBox_KeyDown(object sender, KeyEventArgs e) {
-            if (e.Key == Key.Enter) {
-                string ingredient = EntryLineTextBox.Text;
-
-                System.Windows.Controls.Button testTag = new System.Windows.Controls.Button();
-                testTag.Content = ingredient;
-                TagUserControl testTagUC = new TagUserControl();
-                testTagUC.TagContent(ingredient);
-                TagFieldWrapPanel.Children.Add(testTagUC);
-
-                EntryLineTextBox.Clear();
-            }
-        }
-
-        private void RecipeRequestButton_Click(object sender, RoutedEventArgs e) {
-
-        }
+    private void RecipeRequestButton_Click(object sender, RoutedEventArgs e) {
     }
 }

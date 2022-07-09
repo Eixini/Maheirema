78d751ff9645c96de480ab9eeba24b890bc24179
acidicMercury8 <acidicmercury8@outlook.com>
2022-07-09 14:45:37 +0300

    Fix codestyle

--- src/Eixini.Maheirema.Wpf/App.xaml.cs
+++ src/Eixini.Maheirema.Wpf/App.xaml.cs
@@ -1,15 +1,15 @@
-﻿using System.Windows;
+using System.Windows;
 
-namespace Eixini.Maheirema.Wpf {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
-        protected override void OnStartup(StartupEventArgs e) {
-            base.OnStartup(e);
+namespace Eixini.Maheirema.Wpf;
 
-            var mainWindow = new MainWindow();
-            mainWindow.Show();
-        }
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+    protected override void OnStartup(StartupEventArgs e) {
+        base.OnStartup(e);
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
     }
 }

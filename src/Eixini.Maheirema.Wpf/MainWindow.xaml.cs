3eaec42b3f7a4e5850915dcd543c46489f5a2f86
Eixini <eixi16@gmail.com>
2022-07-09 14:24:26 +0300

    Implement tag logic adding by Enter key

--- src/Eixini.Maheirema.Wpf/MainWindow.xaml.cs
+++ src/Eixini.Maheirema.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Eixini.Maheirema.Wpf {
     /// <summary>
@@ -7,6 +8,24 @@ namespace Eixini.Maheirema.Wpf {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void EntryLineTextBox_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                string ingredient = EntryLineTextBox.Text;
+
+                System.Windows.Controls.Button testTag = new System.Windows.Controls.Button();
+                testTag.Content = ingredient;
+                TagUserControl testTagUC = new TagUserControl();
+                testTagUC.TagContent(ingredient);
+                TagFieldWrapPanel.Children.Add(testTagUC);
+
+                EntryLineTextBox.Clear();
+            }
+        }
+
+        private void RecipeRequestButton_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }

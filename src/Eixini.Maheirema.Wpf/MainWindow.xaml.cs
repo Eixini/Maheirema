d6fd0170f94f242d948ecd6c62a24e6d1624936a
Eixini <eixi16@gmail.com>
2022-07-10 22:30:17 +0300

    Add recipe request event handler
    Currently, when you click on the "Request Recipe" button, a check is made for the presence of tags in the tags field, if there are tags, a collection of ingredients is created for further processing.

--- src/Eixini.Maheirema.Wpf/MainWindow.xaml.cs
+++ src/Eixini.Maheirema.Wpf/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ public partial class MainWindow : Window {
     }
 
     private void EntryLineTextBox_KeyDown(object sender, KeyEventArgs e) {
-        if (e.Key == Key.Enter) {
+        if (e.Key == Key.Enter && EntryLineTextBox.Text != "") {
             var ingredient = EntryLineTextBox.Text;
 
             var testTag = new Button {
@@ -29,5 +29,24 @@ public partial class MainWindow : Window {
     }
 
     private void RecipeRequestButton_Click(object sender, RoutedEventArgs e) {
+
+        if (TagFieldWrapPanel.Children.Count == 0) {
+            MessageBox.Show("Вы не ввели ингредиенты!");
+            return;
+        }
+        var ingredients = new List<string>();
+
+        foreach (var tag in TagFieldWrapPanel.Children) {
+            
+            var grid = tag as TagUserControl;
+            var ingredient = grid?.TagButton.Content as string;
+
+            ingredients.Add(ingredient);
+        }
+    }
+
+    private void DirectoryCheck() {
+        var systemUserName = Environment.UserName as string;
     }
 }
+

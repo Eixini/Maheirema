d4a4036e00776e59d15aec21fc18ad684130262e
Eixini <eixi16@gmail.com>
2022-07-21 01:36:03 +0300

    Add page available recipes
    After comparing the entered ingredients with the ingredients of the recipes, the recipes that match the entered ingredients are added to the result and available to the user for viewing

---
+++ src/Eixini.Maheirema.Wpf/RecipeButton.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Documents;
+using System.IO;
+using System.Windows.Input;
+
+
+namespace Eixini.Maheirema.Wpf;
+public partial class RecipeButton : UserControl {
+    public RecipeButton() {
+        InitializeComponent();
+    }
+
+    public void SetText(string text) {
+        RecipeButtonElement.Content = text;
+    }
+
+    // to store the path to the recipe file
+    public string RecipePath { get; set; }
+
+    private void ShowRecipe(object sender, RoutedEventArgs e) {
+
+        //MessageBox.Show(sender.GetType().ToString());
+
+        //Getting text from a recipe file
+        //TextRange recipeFile = new TextRange(RecipeText.Document.ContentStart, RecipeText.Document.ContentEnd);
+        //FileStream recipeFileStream = new FileStream(RecipePath, FileMode.Open);
+        //recipeFile.Load(recipeFileStream, DataFormats.Rtf);
+
+
+    }
+
+}
+
